34d6908868c8e905ebcfe50cb4e6d6df78ef6b84
Danish <sheikhd@rknec.edu>
2025-03-22 16:07:13 +0530

    c

--- Src_Code/web_functions.py
+++ Src_Code/web_functions.py
@@ -10,7 +10,7 @@ def load_data():
     # This function returns the preprocessed data
 
     # Load the Diabetes dataset into DataFrame.
-    df = pd.read_csv('/Users/danishsheikh/Desktop/Human_Stress_Detection-main/Src_Code/Stress.csv')
+    df = pd.read_csv('Src_Code/Stress.csv')
 
     # Rename the column names in the DataFrame.
     df.rename(columns={"t": "bt", }, inplace=True)
